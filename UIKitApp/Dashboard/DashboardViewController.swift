49faf44ffdb3f60f35bd7b32c6cee35d5eacb26a
Raúl Escalante <raul.escalante@elektra.com.mx>
2022-09-23 10:16:51 -0500

    Add changes on dashboard

--- UIKitApp/Dashboard/DashboardViewController.swift
+++ UIKitApp/Dashboard/DashboardViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class DashboardViewController: UIViewController {
 
+    @IBOutlet weak var nameUser: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let nameData = UserDefaults.standard.string(forKey: "name") else { return }
+        nameUser.text = "Welcome, \(nameData)"
     }
 }
